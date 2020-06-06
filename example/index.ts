d8cdf5d7bbde78abf00894f0d5585be6039a3ee2
burhan ahmed <burhanahmeed@gmail.com>
2020-06-06 20:59:58 +0700

    example: update example file

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,4 @@
-import { Time, timezone } from "../mod.ts";
+import { Time, timezone } from "https://denopkg.com/burhanahmeed/time.ts/mod.ts";
 
 console.log('Jakarta Timezone: ', new Time().timezone('Asia/Jakarta'))
 console.log('Singapore Timezone: ', new Time().timezone('Asia/Singapore'))
